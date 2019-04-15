fc47c985641a293fa4f94f62b66df4251d531807
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-04-15 16:57:30 +0300

    finished-app

---
+++ pages/posts/_id.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <article>
+            <h1>{{ post.title }}</h1>
+            <p>{{ post.content }}</p>
+        </article>
+        <aside>
+            <h3>Posts you might enjoy</h3>
+            <li v-for="related in relatedPosts">
+                <nuxt-link :to="`/posts/${related.id}`">
+                    {{related.title}}
+                </nuxt-link>
+            </li>
+        </aside>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
+        computed: {
+            post () {
+                return this.$store.state.posts.all.find(post => post.id === this.id)
+            },
+            relatedPosts() {
+                return this.$store.state.posts.all.filter(post => post.id !== this.id)
+            }
+        },
+        head(){
+            return{
+                title: this.post.title,
+                meta: [
+                    { name: 'twitter:title', content: this.post.title},
+                    { name: 'twitter:description', content: this.post.content},
+                    { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png'},
+                    { name: 'twitter:card', content: 'summary_large_image'}
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container{
+        display: flex;
+        justify-content: space-between;
+        line-height: 1.5;
+    }
+    article * {
+        margin-bottom: 1rem;
+    }
+    aside{
+        min-width: 280px;
+        max-width: 280px;
+    }
+</style>
---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all : [
+        {
+            id: 'test',
+            title: 'What is test',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu vehicula metus, at condimentum lacus. Aliquam sapien odio, consequat sit amet hendrerit vel, aliquam ut nunc. Nam vehicula blandit orci, sed venenatis magna. Nam varius accumsan scelerisque.'
+        },
+        {
+            id: 'whereIsIt',
+            title: 'Where is the sign in button?',
+            content: 'Phasellus sagittis vulputate ipsum, in eleifend nibh. Fusce justo magna, interdum  ornare sapien eros, at cursus augue pharetra'
+        },
+        {
+            id: 'how',
+            title: 'How can we test',
+            content: 'Etiam eleifend finibus purus in consequat. Morbi consectetur velit sed lorem vestibulum, tortor sit amet, tempus ultricies ligula.'
+        }
+    ]
+});
